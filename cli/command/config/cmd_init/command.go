5b1346ba46450eab8bd97284af35faf0ace15ed6
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-12-29 17:49:56 +0800

    fix: add type to flag

--- cli/command/config/cmd_init/command.go
+++ cli/command/config/cmd_init/command.go
@@ -6,6 +6,7 @@ var Command = &cli.Command{
 	Name: "init",
 	Flags: []cli.Flag{
 		flagForce,
+		flagType,
 	},
 	Usage:     "Initialize config file",
 	ArgsUsage: `"global" | "local" | empty (defaults to "local")`,
