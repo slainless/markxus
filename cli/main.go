6da281a478cffa660130422e5e53c3b57c048b19
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-12-29 17:53:07 +0800

    pffftt

--- cli/main.go
+++ cli/main.go
@@ -20,25 +20,3 @@ func main() {
 		log.Fatal(err)
 	}
 }
-
-// func main() {
-// 	nexusKey := nexus.WithApiKey("75K+pcclj+XVwg8GM5jTrMLXcHMBARdZ9RcDbL3pb+nCvfyS48D6--KlL2Ej4HSG68hbh2--4tVrTFa6dIytXQex2/K0Gw==")
-// 	nexus, err := nexus.NewClient(nexusKey, nexus.WithHTTPDriver(resty.NewRestyClient()))
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	genaiKey := genai.WithApiKey("AIzaSyAgMo6REA1M37g-GclUt9NXYcezGkRNQqw")
-// 	genai, err := genai.NewGenAiClient(context.TODO(), genaiKey)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	markxus := markxus.NewMarkxus(nexus, genai)
-// 	mod, err := markxus.Generate(context.TODO(), "skyrimspecialedition", "68068")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	fmt.Printf("%+v", mod)
-// }
